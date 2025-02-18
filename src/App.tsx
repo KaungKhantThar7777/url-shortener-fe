fd126f4dd2c318d620c3068edbbaaa1ae4962579
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-02-19 02:05:53 +0700

    fix: redirect url

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
 import "./App.css";
 import UrlShortener from "./UrlShortener";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <UrlShortener />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path.length > 1) {
+      setIsLoading(true);
+      const shortCode = path.slice(1);
+
+      fetch(`https://url-shortener-be-u3bk.onrender.com/api/url/${shortCode}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.originalUrl) {
+            window.location.href = data.originalUrl;
+          } else {
+            setError("URL not found");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching redirect URL:", error);
+          setError("Failed to fetch URL");
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, []);
+
+  if (isLoading) return <div>Redirecting...</div>;
+  if (error) return <div>{error}</div>;
+  if (window.location.pathname === "/") return <UrlShortener />;
+
+  return null;
 }
 
 export default App;
